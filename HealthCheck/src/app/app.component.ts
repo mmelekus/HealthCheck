3b390f0a2c842ea857624f5c1746aa4362e9b007
MartinMZen <mmelekus@users.noreply.github.com>
2022-04-22 22:32:26 -0500

    Add project files.

---
+++ HealthCheck/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'HealthCheck';
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
